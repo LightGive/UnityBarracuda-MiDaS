c7f2bc7be0523b4340d8e1221a8d27d6e07f24ff
Akase Matsuura <roi.akase@outlook.jp>
2021-07-26 22:00:59 +0900

    ComputeShaderで画像処理するように変更

--- Assets/_Projects/TestMidas.cs
+++ Assets/_Projects/TestMidas.cs
@@ -6,85 +6,84 @@ using System.Linq;
 
 public class TestMidas : MonoBehaviour
 {
-    [SerializeField]
-    private int width;
-    [SerializeField]
-    private int height;
-    [SerializeField]
-    private RawImage debugNormalImage;
-    [SerializeField]
-    private RawImage depthImage;
-    [SerializeField]
-    private NNModel model;
+	private const int ImageSize = 256;
 
-    private Color[] tmpColor = new Color[256 * 256];
-    private RenderTexture depthTex = null;
-    private WebCamTexture webCamTexture = null;
+	[SerializeField]
+	private RawImage debugNormalImage;
+	[SerializeField]
+	private RawImage depthImage;
+	[SerializeField]
+	private NNModel model;
+	[SerializeField]
+	private ComputeShader preProcessShader;
+	[SerializeField]
+	private ComputeShader postProcessShader;
 
-    IEnumerator Start()
+	private ComputeBuffer tensorBuffer;
+	private RenderTexture depthTex = null;
+	private WebCamTexture webCamTexture = null;
+	private IWorker woker;
+
+	IEnumerator Start()
+	{
+		if (WebCamTexture.devices.Length == 0)
+		{
+			Debug.Log("Cameraのデバイスが無い");
+			yield break;
+		}
+
+		yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
+		if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
+		{
+			Debug.Log("カメラの許可が必要");
+		}
+
+		WebCamDevice device = WebCamTexture.devices[0];
+		webCamTexture = new WebCamTexture(device.name);
+		debugNormalImage.texture = webCamTexture;
+		webCamTexture.Play();
+
+		woker = ModelLoader.Load(model).CreateWorker();
+		tensorBuffer = new ComputeBuffer(ImageSize * ImageSize * 3, sizeof(float));
+
+		depthTex = new RenderTexture(256, 256, 0, RenderTextureFormat.ARGBFloat);
+		depthImage.texture = depthTex;
+
+	}
+
+
+	private void LateUpdate()
+	{
+		if (webCamTexture == null || depthTex == null)
+		{
+			return;
+		}
+
+		//var input = new Tensor(1, ImageSize, ImageSize, 3);
+		int kernelID = preProcessShader.FindKernel("Preprocess");
+		preProcessShader.SetTexture(kernelID, "_Texture", webCamTexture);
+		preProcessShader.SetBuffer(kernelID, "_Tensor", tensorBuffer);
+		preProcessShader.Dispatch(kernelID, ImageSize / 8, ImageSize / 8, 1);
+
+		using (var tensor = new Tensor(1, ImageSize, ImageSize, 3, tensorBuffer))
+        {
+			woker.Execute(tensor);
+        }
+
+		var output = woker.PeekOutput();
+		var tex = output.ToRenderTexture(0, 0, 1.0f / 32, 0.5f);
+		Graphics.Blit(tex, depthTex);
+		Destroy(tex);
+	}
+
+    private void OnDestroy()
     {
-        if (WebCamTexture.devices.Length == 0)
-        {
-            Debug.Log("Cameraのデバイスが無い");
-            yield break;
-        }
+		tensorBuffer?.Dispose();
+		tensorBuffer = null;
 
-        yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
-        if (!Application.HasUserAuthorization(UserAuthorization.WebCam))
-        {
-            Debug.Log("カメラの許可が必要");
-        }
+		woker?.Dispose();
+		woker = null;
 
-        WebCamDevice device = WebCamTexture.devices[0];
-        webCamTexture = new WebCamTexture(device.name, width, height);
-        debugNormalImage.texture = webCamTexture;
-        webCamTexture.Play();
-
-        depthTex = new RenderTexture(256, 256, 0,RenderTextureFormat.ARGBFloat);
-        depthImage.texture = depthTex;
-    }
-
-
-    private void LateUpdate()
-    {
-        if(webCamTexture == null)
-        {
-            return;
-        }
-
-        using var input = new Tensor(1, 256, 256, 3);
-        for (var y = 0; y < 256; y++)
-        {
-            for (var x = 0; x < 256; x++)
-            {
-                var tx = x * webCamTexture.width / 256;
-                var ty = y * webCamTexture.height / 256;
-                var c = webCamTexture.GetPixel(tx, ty);
-                input[0, 255 - y, x, 0] = c.r;
-                input[0, 255 - y, x, 1] = c.g;
-                input[0, 255 - y, x, 1] = c.b;
-            }
-        }
-
-        using var worker = ModelLoader.Load(model).CreateWorker();
-        worker.Execute(input);
-        var output = worker.PeekOutput();
-
-        //1,1,256,256
-        //1,1,1,10
-
-        var tex = new Texture2D(256, 256);
-        for (var y = 0; y < 256; y++)
-        {
-            for (var x = 0; x < 256; x++)
-            {
-                var o = output[0, 0, x, 255 - y]/1000.0f;
-                tmpColor[(y * 256) + x] = new Color(o, o, o);
-            }
-        }
-
-        tex.SetPixels(tmpColor);
-        tex.Apply();
-        Graphics.Blit(tex, depthTex);
+		depthTex = null;
     }
 }
